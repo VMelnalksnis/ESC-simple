1a922973c196cd31956f07e5a4c6c805088b541b
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2018-04-10 15:21:21 +0300

    Updated SOFTWARE/Control
    The first version didn't work - it blinked randomly while a control signal was applied, and with a constant frequency when the control signal was removed.
    
    The program was restructured to run entirely in the ISR, and the startup check and timer1 overflow error was removed. More documentation was also added.

--- SOFTWARE/Control/control.h
+++ SOFTWARE/Control/control.h
@@ -1,9 +1,10 @@
 /*
- * control.h
+ *	control.h
  *
- * Created: 2018-04-09 14:12:05
- *  Author: Valters Melnalksnis
- */ 
+ *	Created	: 2018-04-09 14:12:05
+ *	Author	: Valters Melnalksnis
+ *	PCB		: V1A
+ */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -19,17 +20,28 @@
 //	-) INT0		- External Interrupt 0 Input
 //	-) PCINT18	- Pin Change Interrupt 18
 
+
 #define TEST_PIN_1		PINB1
 #define TEST_PIN_2		PINB2
 #define TEST_DDR		DDRB
 #define TEST_PORT		PORTB
 
+#define CTRL_PINS		PIND
 #define CTRL_PIN		PIND2
 #define CTRL_DDR		DDRD
 #define CTRL_PORT		PORTD
 
+/*	Timer1 output compare A value corresponding to the minimum pulse width.	*/
 #define CTRL_MIN		16000
+/*	Timer1 output compare A value corresponding to the maximum pulse width.	*/
 #define CTRL_MAX		32000
+
+#define CTRL_SCALE		800
+
+/*	Timer0 control register value for setting a pre-scaler of 1024.	*/
+#define TIMER0_PRESC	((1 << CS02) | (0 << CS01) | (1 << CS00))
+/*	Timer1 control register value for setting a pre-scaler of 1.	*/
+#define TIMER1_PRESC	((0 << CS12) | (0 << CS11) | (1 << CS10))
 
 void InitIO(void);
 void InitTimers(void);
--- SOFTWARE/Control/main.c
+++ SOFTWARE/Control/main.c
@@ -1,136 +1,166 @@
 /*
  *	main.c
  *
- *	Created	: 08.04.2018 14:47:38
+ *	Created	: 08.04.2018
  *	Author	: Valters Melnalksnis
  *	PCB		: V1A
  */
 
 #include "control.h"
 
-volatile uint16_t ctrl_length = 0;
-volatile uint8_t ctrl_changed = 0;
-volatile uint8_t blink_ovrflw = 0;
-volatile uint8_t blink_counter = 0;
+/*	This counter is used to blink an LED on TEST_PIN_1.
+ *	The frequency of the blinking is dependent on the control signal.
+ */
+volatile uint8_t overflow_counter = 0;
+/*	The above counter is compared to this value to decided whether TEST_PIN_1
+ *	should be toggled. This value is proportional to the width of the last
+ *	control signal pulse.
+ */
+volatile uint8_t blink_period = 0;
+
 
 int main(void)
 {
-	// Disable Global Interrupts.
-	cli();
-	
+	/*	Initialization functions.	*/
 	InitIO();
 	InitTimers();
 	InitInterrupts();
-	
-	// Set 2nd test pin HIGH, indicating startup state.
-	TEST_PORT ^= (1 << TEST_PIN_2);
-	// Wait for control pin to be LOW.
-	while ((PIND & (1 << CTRL_PIN)) == 1)
-	{
-		;
-	}
 
-	// Set 2nd test pin LOW, indicating normal operation.
-	TEST_PORT ^= (1 << TEST_PIN_2);
-	
-	// Enable Global Interrupts.
+	/*	Enable global interrupts.	*/
 	sei();
-
+	
+	/*	Run this forever.	*/
 	while (1)
 	{
-		if (ctrl_changed == 1)
-		{
-			// Clamp the control signal to the min/max values.
-			if (ctrl_length < CTRL_MIN)
-				ctrl_length = CTRL_MIN;
+		/*	Do nothing since everything is handled is ISR.	*/
+	}/*	End while(1).	*/
 
-			if (ctrl_length > CTRL_MAX)
-				ctrl_length = CTRL_MAX;
-			
-			// Blink interval from 0.3 to 0.6 seconds.
-			blink_ovrflw = ctrl_length / 800;
-			
-			// Toggle LED
-			if (blink_counter >= blink_ovrflw)
-			{
-				TEST_PORT ^= (1 << TEST_PIN_1);
-				blink_counter = 0;
-			}
-		}
-	}
-}
+}/*	End main(void).	*/
 
-void InitIO()
+/*	Initialize all the used inputs/outputs.	*/
+void InitIO(void)
 {
-	// Set test pins as outputs.
+	/*	Set the test pins as outputs.	*/
 	TEST_DDR = (1 << TEST_PIN_1) | (1 << TEST_PIN_2);
-	// Set outputs to LOW.
+	/*	Initialize the test outputs to LOW.	*/
 	TEST_PORT = (0 << TEST_PIN_1) | (0 << TEST_PIN_2);
-	// Set control pin as an input.
+	/*	Set control signal pin as an input.	*/
 	CTRL_DDR = 0;
-	// Disable pull-up resistor
+	/*	Disable internal pull-up resistor for the control signal.	*/
 	CTRL_PORT = 0;
 }
 
-void InitTimers()
+/*	Initialize all the used timers/counters.
+ *	
+ *	Timer1 is used to measure the width of the control signal pulse.
+ *	Since the maximum width of the pulse is 2ms, the timer can be used with
+ *	a pre-scaler of 1 for maximum resolution. The output compare values A and B
+ *	are set to the minimum and maximum pulse width, respectively.
+ *	
+ *	Timer0 is used to blink an LED on TEST_PIN_1.
+ *	Since the blinking frequency change should be distinguishable by the 
+ *	human eye, a pre-scaler of 1024 is used for maximum period of 4.2s when
+ *	using an 8-bit overflow counter.
+ */
+void InitTimers(void)
 {
-	// Disable Compare Output Mode, Normal Mode of Operation.
+	/*	Initialize Timer1 - disable compare output mode, set normal mode of operation,
+		select no clock source.	*/
 	TCCR1A = 0;
-	// Normal Mode of Operation, Disable Input Capture, No clock source.
 	TCCR1B = 0;
-	// Disable Force Output Compare.
 	TCCR1C = 0;
-	// Enable Overflow Interrupt.
-	TIMSK1 = (1 << TOIE1);
-	
-	// Disable Compare Output Mode, Normal Mode of Operation.
+	/*	Set Output Compare A to min pulse width (1ms).	*/
+	OCR1A = CTRL_MIN;
+	/*	Set Output Compare B to max pulse width (2ms).	*/
+	OCR1B = CTRL_MAX;
+
+	/*	Initialize Timer0 - disable compare output mode, set normal mode of operation,
+		select clock with a pre-scaler of 1024.	*/
 	TCCR0A = 0;
-	// Disable Force Output Compare, 1024 pre-scaler.
-	TCCR0B = (1 << CS02) | (0 << CS01) | (1 << CS00);
-	// Enable Overflow interrupt.
-	TIMSK0 = (0 << OCIE0B) | (0 << OCIE0A) | (1 << TOIE0);
+	TCCR0B = TIMER0_PRESC;
 }
 
-void InitInterrupts()
+/*	Initialize all the used interrupts.
+ *	
+ *	Timer0 overflow interrupt is used control the blinking frequency of TEST_PIN_1.
+ *	
+ *	Timer1 output compare interrupts are used to clamp the control signal pulse width
+ *	to the predefined minimum and maximum values.
+ *
+ *	External interrupt 0 is used to detect the rising and falling edge of the control
+ *	signals pulse.
+ */
+void InitInterrupts(void)
 {
-	// Any logical change on INT0 generates an interrupt request.
-	EICRA = (1 << ISC00);
-	// Enable External Interrupt Request 0.
+	/*	Enable Overflow interrupt for Timer0.	*/
+	TIMSK0 = (1 << TOIE0);
+	/*	Enable output compare interrupts for Timer1.	*/
+	TIMSK1 = (1 << OCIE1B) | (1 << OCIE1A) | (0 << TOIE1);
+
+	/*	Any logical change on INT0 generates an interrupt request.	*/
+	EICRA = (0 << ISC01) | (1 << ISC00);
+	/*	Enable External Interrupt Request 0.	*/
 	EIMSK = (1 << INT0);
 }
 
+/*	This is the Timer0 overflow interrupt service routine; runs every time the Timer0
+ *	overflows. Each time overflow_counter is incremented and checked against
+ *	blink_perdiod; if the former is greater than the latter, TEST_PIN_1 is toggled and
+ *	overflow_counter is reset.
+ */
+ISR(TIMER0_OVF_vect)
+{
+	overflow_counter++;
+	
+	if (overflow_counter >= blink_period)
+	{
+		/*	Toggle the logic state of TEST_PIN_1.	*/
+		TEST_PORT ^= (1 << TEST_PIN_1);
+		/*	Reset the counter.	*/
+		overflow_counter = 0;
+	}
+}
+
+/*	This is the external interrupt 0 interrupt service routine; runs every time the 
+ *	logic level of the corresponding pin changes. If a rising edge is detected, Timer1
+ *	is started, otherwise the pulse width is clamped between minimum and maximum, and
+ *	blink_period is set according to the value of Timer1.
+ */
 ISR(INT0_vect)
 {
-	if ((PIND & (1 << CTRL_PIN)) == 1)
+	if ((CTRL_PINS & (1 << CTRL_PIN)) == 1)
 	{
-		// Control pin changed from LOW to HIGH.
-		// Start timer by selecting clock source with no pre-scaler.
-		TCCR1B = (1 << CS10);
+		/*	Control signals rising edge; start Timer1.	*/
+		TCCR1B = TIMER1_PRESC;
 	}
 	else
 	{
-		// Stop timer.
+		/*	Control signals falling edge.	*/
+		if ((TIFR1 & (1 << OCF1A)) == 0)
+		{
+			/*	Timer1 didn't exceed minimum pulse width.
+				Clamp the blink period to the minimum value.	*/
+			blink_period = CTRL_MIN / CTRL_SCALE;
+		}
+		else if ((TIFR1 & (1 << OCF1B)) == 1)
+		{
+			/*	Timer1 exceeded maximum pulse width.
+				Clamp the blink period to the maximum value and clear the Timer1
+				output compare B flag.	*/
+			blink_period = CTRL_MAX / CTRL_SCALE;
+			TIFR1 = (1 << OCF1B);
+		}
+		else
+		{
+			/*	Timer1 is within bounds.
+				Clear the Timer1 output compare A flag.	*/
+			blink_period = (TCNT1 / CTRL_SCALE);
+			TIFR1 = (1 << OCF1A);
+		}
+		
+		/*	Select no clock source for Timer1 to stop it.	*/
 		TCCR1B = 0;
-		// Read current TIMER1 value.
-		ctrl_length = TCNT1;
-		// Clear TIMER1 value.
+		/*	Clear Timer1.	*/
 		TCNT1 = 0;
-		// Set the control changed flag.
-		ctrl_changed = 1;
-		// Set 2nd test pin LOW, indicating normal operation.
-		TEST_PORT &= ~(1 << TEST_PIN_2);
 	}
-}
-
-ISR(TIMER0_OVF_vect)
-{
-	blink_counter++;
-}
-
-ISR(TIMER1_OVF_vect)
-{
-	// Disable Global Interrupts.
-	cli();
-	// Set 2nd test pin HIGH, indicating error state.
-	TEST_PORT |= (1 << TEST_PIN_2);
 }
